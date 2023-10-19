b7211d68d5e194f159ffbbdc274be692eac947e6
kaushal kumar <60603249+kaushalkumar50009@users.noreply.github.com>
2023-10-20 00:19:57 +0530

    adding copy color code functionality

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+let randomCode;
+
 const changeColor = () => {
   const randomNumber = Math.floor(Math.random() * 16777215);
-  //   console.log(randomNumber);
-
-  const randomCode = "#" + randomNumber.toString(16);
-  //   console.log(randomCode);
-
+  randomCode = "#" + randomNumber.toString(16);
   document.body.style.backgroundColor = randomCode;
-
   document.getElementById("colorCode").innerText = randomCode;
-
-  navigator.clipboard.writeText(randomCode);
 };
 
+function copyToClipboard() {
+  if (randomCode) {
+    navigator.clipboard
+      .writeText(randomCode)
+      .then(() => {
+        document.getElementById("copy_btn").innerText = "copied";
+        setTimeout(() => {
+          document.getElementById("copy_btn").innerText = "copy";
+        }, 2000);
+      })
+      .catch((error) => {
+        console.error("Copy to clipboard failed:", error);
+      });
+  }
+}
+
 let btn = document.getElementById("btn");
-btn.addEventListener("click", changeColor);
+let copy_btn = document.getElementById("copy_btn");
+btn.addEventListener("click", changeColor); // Change background color
+copy_btn.addEventListener("click", copyToClipboard); // Copy color code
 
 changeColor();
